624f9e4dc0ebf662590d6d499798185dc41bde7b
niha <niha8515@yahoo.com>
2022-02-13 09:49:08 -0800

    main activity kotlin code

--- app/src/main/java/com/example/kotlinhw6/MainActivity.kt
+++ app/src/main/java/com/example/kotlinhw6/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.kotlinhw6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlinhw6.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        var view = binding.root
+        setContentView(view)
+
+        binding.q1Btn.setOnClickListener {
+            val intent = Intent(this ,Information::class.java)
+            startActivity(intent)
+        }
+        binding.q2Btn.setOnClickListener {  }
+        binding.q3Btn.setOnClickListener {  }
     }
 }
